b13f95914d91601997bc9af3f308b7392e794515
fakhri ramdani <muhammadlukman09@gmail.com>
2019-09-26 09:54:39 +0700

    project keenam

---
+++ src/pboul/pkg10115229/latihan6/kambingstatic/PBOUL10115229Latihan6KambingStatic.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan6.kambingstatic;
+
+/**
+ *
+ * @author Lukman
+ * NIM          : 10115229
+ * NAMA         : MUHAMMAD LUKMAN HAKIM
+ * HARITANGGAL  : SABTU, 21 SEPTEMBER 2019
+ * LOG
+ */
+public class PBOUL10115229Latihan6KambingStatic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    public class Mamalia {
+        // Variabel jumlahKambing dideklarasikan sebagai statik
+        
+    }
+    
+    public class KambingStatic {
+        // NAMA_KAMBING sebagai konstanta
+        public static final String NAMA_KAMBING = "MIDUN";
+        
+        public static void main(String[] args) {
+            Mamalia.jumlahKambing = 485000;
+            System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + Mamalia.jumlahKambing);
+        }
+    }
+    }
+    
+}
